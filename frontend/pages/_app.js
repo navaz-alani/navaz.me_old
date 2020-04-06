f80eb5dc299b003780b481d23d8e500a1d242e14
Navaz Alani <48425461+navaz-alani@users.noreply.github.com>
2020-04-06 16:34:10 -0400

    removed rougue import

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css";
-import "../components/home/styles.css"
 
 const Site = ({ Component, pageProps }) => {
     return <Component {...pageProps}/>;
