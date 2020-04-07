afc2270d196be51aa1917a42e621147cf9e1a140
Navaz Alani <nalani@uwaterloo.ca>
2020-04-06 23:11:50 -0400

    fixed nginx installer bug

--- nginxConfInstaller.sh
+++ nginxConfInstaller.sh
@@ -3,7 +3,7 @@
 pre_config_file=nginx.conf
 tmp=/tmp/navaz.me
 tmp_conf=$tmp/$pre_config_file
-nginx_conf=/etx/nginx/conf.d/navaz.me.conf
+nginx_conf=/etc/nginx/conf.d/navaz.me.conf
 
 if [[ -z $1 ]]; then
   echo "navaz.me: [error] hosting domiain not specified";
@@ -17,7 +17,8 @@ elif [[ ! -f $pre_config_file ]]; then
 fi
 
 # Set hosting domain, create nginx configuration
-sed "s/SITE_DOMAIN/$1" $pre_config_file > $tmp_conf;
+mkdir $tmp;
+sed "s/SITE_DOMAIN/$1/" $pre_config_file > $tmp_conf;
 # Copy nginx configuration over
 cp "$tmp_conf"  $nginx_conf;
 # Clean up tmp
